da6a717d366a7634b59d37fc513e9660ebd5bc0b
CojocaruSefan <stefan_c97@yahoo.com>
2018-11-05 12:29:04 +0200

    testin

--- towerOfHanoi/src/main/java/TowersOfHanoi.java
+++ towerOfHanoi/src/main/java/TowersOfHanoi.java
@@ -10,16 +10,6 @@ public class TowersOfHanoi {
     private static final int NUMBER_OF_PEGS = 5;
 
 
-    public void solve(int n, String start, String auxiliary, String end) {
-        if (n == 1) {
-            System.out.println(start + " -> " + end);
-        } else {
-            solve(n - 1, start, end, auxiliary);
-            System.out.println(start + " -> " + end);
-            solve(n - 1, auxiliary, start, end);
-        }
-    }
-
     public static void main(String[] args) {
         TowersOfHanoi towersOfHanoi = new TowersOfHanoi();
 //        System.out.print("Enter number of discs: ");
@@ -28,11 +18,16 @@ public class TowersOfHanoi {
 //        towersOfHanoi.solve(discs, "A", "B", "C");
 
         towersOfHanoi.initializeGame(NUMBER_OF_DISKS, NUMBER_OF_PEGS);
+        while (!towersOfHanoi.isFinal()) {
+            towersOfHanoi.solve();
+        }
+        /*
         for (Peg peg : pegs) {
             if (peg.getDisks() != null) {
                 System.out.println(peg.getNumber() + " " + peg.getDisks());
             } else System.out.println(peg.getNumber() + " 0");
         }
+        */
     }
 
     public void solve() {
@@ -41,31 +36,44 @@ public class TowersOfHanoi {
 
 
         Peg sourcePeg = pegs.stream()
-                .filter(peg->peg.getNumber() == sourcePegNumber).findFirst().get();
+                .filter(peg -> peg.getNumber() == sourcePegNumber).findFirst().get();
         Peg destinationPeg = pegs.stream()
-                .filter(peg->peg.getNumber() == destinationPegNumber).findFirst().get();
-        if(sourcePeg != destinationPeg){
+                .filter(peg -> peg.getNumber() == destinationPegNumber).findFirst().get();
+        if (sourcePeg != destinationPeg && !sourcePeg.getDisks().isEmpty()) {
             moveDisk(sourcePeg, destinationPeg);
+            if (sourcePeg.getDisks() != null) {
+                System.out.println(sourcePeg.getNumber() + " " + sourcePeg.getDisks());
+            } else System.out.println(sourcePeg.getNumber() + " 0");
+            if (destinationPeg.getDisks() != null) {
+                System.out.println(destinationPeg.getNumber() + " " + destinationPeg.getDisks());
+            } else System.out.println(destinationPeg.getNumber() + " 0");
         }
+
     }
 
     public void moveDisk(Peg sourcePeg, Peg destinationPeg) {
-        if (isMoveOk(sourcePeg.getDisks().peek(), destinationPeg)) {
-            Disk diskToBeMoved = sourcePeg.getDisks().pop();
-            destinationPeg.getDisks().push(diskToBeMoved);
-        }
+        if (!sourcePeg.getDisks().isEmpty())
+            if (isMoveOk(sourcePeg.getDisks().peek(), destinationPeg)) {
+                Disk diskToBeMoved = sourcePeg.getDisks().pop();
+                destinationPeg.getDisks().push(diskToBeMoved);
+            }
     }
 
     public boolean isMoveOk(Disk disk, Peg peg) {
-        if (disk.getWidth() > peg.getDisks().pop().getWidth()) {
-            return false;
-        }
+        if (!peg.getDisks().isEmpty())
+            if (disk.getWidth() > peg.getDisks().pop().getWidth()) {
+                return false;
+            }
         return true;
     }
 
     private void initializeGame(int numberOfDisks, int numberOfPegs) {
         for (int index = 1; index <= numberOfPegs; index++) {
-            pegs.add(new Peg(index));
+
+            Peg peg = new Peg(index);
+            peg.setDisks(new Stack<Disk>());
+            pegs.add(peg);
+
         }
         Stack<Disk> disks = new Stack<>();
         for (int index = numberOfDisks; index >= 1; index--) {
@@ -74,5 +82,18 @@ public class TowersOfHanoi {
         Peg firstPeg = pegs.stream()
                 .filter(peg -> peg.getNumber() == 1).findFirst().get();
         firstPeg.setDisks(disks);
+    }
+
+    public boolean isFinal() {
+        Peg lastPeg = pegs.stream().filter(peg -> peg.getNumber() == NUMBER_OF_PEGS).findFirst().get();
+        if (lastPeg.getDisks().isEmpty()) {
+            return false;
+        }
+        for (Peg p : pegs) {
+            if (!p.getDisks().isEmpty() && p.getNumber() != NUMBER_OF_PEGS)
+                return false;
+        }
+        return true;
+
     }
 }
